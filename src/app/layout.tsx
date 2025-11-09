9cb3027b4ab41c96f203280cb5672bdf3df98d7b
Alex singer <dano.alexper@gmail.com>
2025-11-09 18:18:44 +0000

    EstudianteChambeaPorfa

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import NextAuthProvider from '@/components/providers/NextAuthProvider';
+import '@/lib/firebase';
 
 export const metadata: Metadata = {
   title: 'LaSalle Gestiona Portal',
