5d05dfa30cd95b02e245fc707cb0bb6611bca7e7
Alex singer <dano.alexper@gmail.com>
2025-11-12 21:56:52 +0000

    EstudianteNegasGod666

--- src/app/estudiante/page.tsx
+++ src/app/estudiante/page.tsx
@@ -1,14 +1,13 @@
- "use client";
+'use client';
 
 import React from 'react';
 import { useAuth } from '@/components/providers/NextAuthProvider';
-import DashboardTemplate from '@/components/templates/DashboardTemplate';
 
 const EstudiantePage = () => {
   const { user } = useAuth();
 
   return (
-    <DashboardTemplate>
+    <> 
       {user ? (
         <div>
           <h1>Bienvenido, {user.name}</h1>
@@ -17,7 +16,7 @@ const EstudiantePage = () => {
       ) : (
         <p>Cargando...</p>
       )}
-    </DashboardTemplate>
+    </>
   );
 };
 
